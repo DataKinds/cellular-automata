2fea9f6bd2c294ff3f10299d3ba3a5eac2314a79
T L <tslimkemann42@gmail.com>
2018-02-17 15:11:42 -0700

    generic types

--- World.hs
+++ World.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module World where
 
 import Data.List
@@ -12,16 +14,20 @@ class World dim where
     down :: dim -> dim
     left :: dim -> dim
     right :: dim -> dim
-    write :: Cell -> dim -> dim
+    write :: a -> dim -> dim
+    getNeighborhood :: dim -> dim
 
-data OneD = OneD [Cell] Cell [Cell]
-instance World OneD where
+data OneD a = OneD [a] a [a] deriving (Functor)
+instance World (OneD Cell) where
     left (OneD (l:ls) c rs) = OneD ls l (c:rs)
     right (OneD ls c (r:rs)) = OneD (c:ls) r rs
     up = id
     down = id
     write c' (OneD ls c rs) = OneD ls c' rs
-instance Show OneD where
+    getNeighborhood (OneD ls c rs) = OneD (take radius ls) c (take radius rs)
+        where
+            radius = 1
+instance Show (OneD Cell) where
     show (OneD ls c rs) = (listString $ reverse shortLs) ++ "  " ++ (show c) ++ "  " ++ (listString shortRs)
         where
             shortLs = take 4 ls
@@ -29,23 +35,27 @@ instance Show OneD where
             listString l = intercalate " " $ map show l
 
 
-data TwoD = TwoD [OneD] OneD [OneD]
-instance World TwoD where
+data TwoD a = TwoD [OneD a] (OneD a) [OneD a] deriving (Functor)
+instance World (TwoD Cell) where
     up (TwoD (u:us) c ds) = TwoD us u (c:ds)
     down (TwoD us c (d:ds)) = TwoD (c:us) d ds
     left (TwoD us c ds) = TwoD (map left us) (left c) (map left ds)
     right (TwoD us c ds) = TwoD (map right us) (right c) (map right ds)
     write c' (TwoD us c ds) = TwoD us (write c' c) ds
-instance Show TwoD where
+    getNeighborhood (TwoD us c ds) = TwoD (map getNeighborhood (take radius us)) (getNeighborhood c) (map getNeighborhood (take radius ds))
+        where
+            radius = 1
+instance Show (TwoD Cell) where
     show (TwoD us c ds) = (listString $ reverse shortUs) ++ "\n\n" ++ (show c) ++ "\n\n" ++ (listString shortDs)
         where
             shortUs = take 4 us
             shortDs = take 4 ds
             listString l = intercalate "\n" $ map show l
 
+
 defaultCell :: Cell
 defaultCell = Off
-genOneD :: OneD
+genOneD :: OneD Cell
 genOneD = OneD (repeat defaultCell) defaultCell (repeat defaultCell)
-genTwoD :: TwoD
+genTwoD :: TwoD (OneD Cell)
 genTwoD = TwoD (repeat genOneD) genOneD (repeat genOneD)
