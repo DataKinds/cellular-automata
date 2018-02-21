61c961684b0d25e73df5b7e11b9352a2048a1147
T L <tslimkemann42@gmail.com>
2018-02-20 22:26:22 -0700

    the types work

--- World.hs
+++ World.hs
@@ -9,55 +9,49 @@ instance Show Cell where
     show On = "◆"
     show Off = "◇"
 
-class World a where
-    up :: a -> a
-    down :: a -> a
-    left :: a -> a
-    right :: a -> a
-    getNeighborhood :: a -> a
-data Dim a = Dim [a] a [a] deriving (Functor)
-newtype OneD = OneD (Dim Cell)
-newtype TwoD = TwoD (Dim OneD)
+data World a = BaseDim [Cell] Cell [Cell] | HigherDim [World a] (World a) [World a]
+    deriving (Functor)
 
-instance World (OneD) where
-    left (OneD (Dim (l:ls) c rs)) = OneD (Dim ls l (c:rs))
-    right (OneD (Dim ls c (r:rs))) = OneD (Dim (c:ls) r rs)
-    up = id
-    down = id
-    getNeighborhood (OneD (Dim ls c rs)) = OneD (Dim (take radius ls) c (take radius rs))
+instance Show (World a) where
+    show (BaseDim l c r) = (listString $ reverse shortLs) ++ "  " ++ (show c) ++ "  " ++ (listString shortRs)
         where
-            radius = 1
-instance Show (OneD) where
-    show (OneD (Dim ls c rs)) = (listString $ reverse shortLs) ++ "  " ++ (show c) ++ "  " ++ (listString shortRs)
+            shortLs = take 4 l
+            shortRs = take 4 r
+            listString ls = intercalate " " $ map show ls
+    show (HigherDim u c d) = (listString $ reverse shortUs) ++ "\n\n" ++ (show c) ++ "\n\n" ++ (listString shortDs)
         where
-            shortLs = take 4 ls
-            shortRs = take 4 rs
-            listString l = intercalate " " $ map show l
+            shortUs = take 4 u
+            shortDs = take 4 d
+            listString ls = intercalate "\n" $ map show ls
 
+left :: World a -> World a
+left (BaseDim (l:ls) c rs) = BaseDim ls l (c:rs)
+left (HigherDim us c ds) = HigherDim (map left us) (left c) (map left ds)
+right :: World a -> World a
+right (BaseDim ls c (r:rs)) = BaseDim (c:ls) r rs
+right (HigherDim us c ds) = HigherDim (map right us) (right c) (map right ds)
+up :: World a -> World a
+up w@(BaseDim l c r) = w
+up (HigherDim (u:us) c ds) = HigherDim us u (c:ds)
+down :: World a -> World a
+down w@(BaseDim l c r) = w
+down (HigherDim us c (d:ds)) = HigherDim (c:us) d ds
 
---data TwoD a = TwoD [OneD a] (OneD a) [OneD a] deriving (Functor)
-instance World (TwoD) where
-    up (TwoD (Dim (u:us) c ds)) = TwoD (Dim us u (c:ds))
-    down (TwoD (Dim us c (d:ds))) = TwoD (Dim (c:us) d ds)
-    left (TwoD (Dim us c ds)) = TwoD (Dim (map left us) (left c) (map left ds))
-    right (TwoD (Dim us c ds)) = TwoD (Dim (map right us) (right c) (map right ds))
-    getNeighborhood (TwoD (Dim us c ds)) = TwoD (Dim (map getNeighborhood (take radius us)) (getNeighborhood c) (map getNeighborhood (take radius ds)))
-        where
-            radius = 1
-instance Show (TwoD) where
-    show (TwoD (Dim us c ds)) = (listString $ reverse shortUs) ++ "\n\n" ++ (show c) ++ "\n\n" ++ (listString shortDs)
-        where
-            shortUs = take 4 us
-            shortDs = take 4 ds
-            listString l = intercalate "\n" $ map show l
+getNeighborhood :: World a -> World a
+getNeighborhood (BaseDim ls c rs) = BaseDim (take radius ls) c (take radius rs)
+    where
+        radius = 1
+getNeighborhood (HigherDim us c ds) = HigherDim (map getNeighborhood (take radius us)) (getNeighborhood c) (map getNeighborhood (take radius ds))
+    where
+        radius = 1
 
---write :: (World a) => b -> a b -> a b
---write c' w =
---write c' (TwoD us c ds) = TwoD us (write c' c) ds
+write :: Cell -> World a -> World a
+write c' (BaseDim l c r) = BaseDim l c' r
+write c' (HigherDim u c d) = HigherDim u (write c' c) d
 
 defaultCell :: Cell
 defaultCell = Off
-genOneD :: OneD
-genOneD = OneD (Dim (repeat defaultCell) defaultCell (repeat defaultCell))
-genTwoD :: TwoD
-genTwoD = TwoD (Dim (repeat genOneD) genOneD (repeat genOneD))
+genOneD :: World a
+genOneD = BaseDim (repeat defaultCell) defaultCell (repeat defaultCell)
+genTwoD :: World a
+genTwoD = HigherDim (repeat genOneD) genOneD (repeat genOneD)
